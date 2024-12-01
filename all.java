821563c9cff5d22ab52abc206e925f453de12d34
Karthikat2 <128841689+Karthikat2@users.noreply.github.com>
2024-12-01 13:59:13 +0530

    Add files via upload

---
+++ all.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner scan= new Scanner(System.in);
+	    System.out.println("enter your input here");
+	    int num=scan.nextInt();
+	    System.out.println(num);
+	    
+	    String word=scan.next();
+	    System.out.println(word);
+	    char letter=scan.next().charAt(0);
+	    System.out.println(letter);
+		// your code gSsoes here
+
+	}
+}
