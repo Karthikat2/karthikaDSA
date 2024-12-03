9abfa0a8fd5ccd0a843ec491c15c84a8606ce456
Karthikat2 <128841689+Karthikat2@users.noreply.github.com>
2024-12-03 19:48:00 +0530

    Add files via upload

---
+++ probI ytimes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner scan=new Scanner(System.in);
+int x=scan.nextInt();
+int y=scan.nextInt();
+for(int i=1;i<=y;i++){
+    System.out.println(x);
+}
+
+
+	}
+}
