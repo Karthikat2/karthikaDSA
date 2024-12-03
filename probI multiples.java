59de0a53d7ef8eb7d7d5f03f7e02c500f855bb26
Karthikat2 <128841689+Karthikat2@users.noreply.github.com>
2024-12-03 19:46:58 +0530

    Add files via upload

---
+++ probI multiples.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner scan=new Scanner(System.in);
+int x=scan.nextInt();
+for(int i=1;i*x<=1000;i++){
+    System.out.println(i*x);
+}
+
+	}
+}
