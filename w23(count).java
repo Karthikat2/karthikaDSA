873c17f8b86391c66025cfd4d63d8bdeb1dfd6d0
Karthikat2 <128841689+Karthikat2@users.noreply.github.com>
2024-12-09 18:32:39 +0530

    Add files via upload

---
+++ w23(count).java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan=new Scanner(System.in);
+		int n= scan.nextInt();
+		int count=1;
+	for(int i=1;i<=n;i++){
+	    for(int j=1;j<=n;j++){
+	        System.out.print(count);
+	        count++;
+	    }
+	    System.out.println("");
+	}
+	   
+
+	}
+}
