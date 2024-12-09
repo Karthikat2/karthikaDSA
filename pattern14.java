595ea68f47c815c9b5bacacb053c61e0f385a3d1
Karthikat2 <128841689+Karthikat2@users.noreply.github.com>
2024-12-09 18:30:52 +0530

    Add files via upload

---
+++ pattern14.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan=new Scanner(System.in);
+		int n= scan.nextInt();
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print(i);
+		    }
+		    System.out.println("");
+		}
+
+	}
+}
