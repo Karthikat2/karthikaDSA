b21fee8a6d9d81732797340ced586fdce8ae5a2d
Karthikat2 <128841689+Karthikat2@users.noreply.github.com>
2024-12-30 20:50:20 +0530

    Add files via upload

---
+++ gcdof2NUM.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+//trailing zero
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();
+int gcd=1;
+for(int i=2;i<=a;i++){
+    if(a%i==0 && b%i==0){
+        gcd=i;
+    }
+}
+System.out.print(gcd);
+}
+}
