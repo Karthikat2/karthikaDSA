c3000bbe200a8051ca1d9348091af68794a67364
Karthikat2 <128841689+Karthikat2@users.noreply.github.com>
2024-12-30 18:56:16 +0530

    Add files via upload

---
+++ p-14(using count).java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* pattern-14 using count
+if n=3,
+1
+22
+333*/
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+	int count=0;
+		for(int i=1;i<=n;i++){
+		   
+		    for(int j=1;j<=i;j++){
+		        
+		         count=i;
+		    System.out.print(count);
+		       
+		      
+		       
+		    }
+		    System.out.println("");
+		}
+
+	}
+}
